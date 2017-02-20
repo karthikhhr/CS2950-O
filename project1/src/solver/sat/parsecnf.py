6e5e82ef55c7e2a6a0e2d77996f45152be857027
pratultandon <pratultandon@Pratuls-MacBook-Pro.local>
2017-02-20 16:57:00 -0500

    add parser for cnf files

---
+++ project1/src/solver/sat/parsecnf.py
@@ -0,0 +1,17 @@
+def read(filename):
+    f = open("input/simple.cnf", "r")
+    num_vars = 0
+    num_clauses = 0
+    started_reading = False
+    clauses = []
+    for line in f:
+        if started_reading:
+            clause = []
+            for lit in line.split()[:-1]:
+                clause.append(int(lit))
+            clauses.append(clause)
+        if line.split()[0] == 'p':
+            started_reading = True
+            num_vars = int(line.split()[2])
+            num_clauses = int(line.split()[3])
+    return num_vars, clauses
